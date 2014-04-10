0e537146016f01c1041017a61639a44c32bcd99a
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-10 18:18:29 +0300

    Initialize repo

---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+var lr = require('tiny-lr'), // Минивебсервер для livereload
+    gulp = require('gulp'), // Сообственно Gulp JS
+    coffee = require('gulp-coffee'), // Плагин для CoffeScript
+    stylus = require('gulp-stylus'), // Плагин для Stylus
+    livereload = require('gulp-livereload'), // Livereload для Gulp
+    csso = require('gulp-csso'), // Минификация CSS
+    uglify = require('gulp-uglify'), // Минификация JS
+    concat = require('gulp-concat'), // Склейка файлов
+    connect = require('connect'), // Webserver
+    plumber = require('gulp-plumber'),
+    server = lr();
+
+    var paths = {
+      js: ['assets/scripts/js/**/*'],
+      coffee: ['assets/scripts/coffee/**/*'],
+      stylus: ['assets/styles/stylus/**/*'],
+      css: ['assets/styles/css/**/*'],
+      pages: ['assets/**/*.html'],
+      images: ['assets/img/**/*']
+    };
+
+    gulp.task('http-server', function() {
+      connect()
+      .use(require('connect-livereload')())
+      .use(connect.static('./public'))
+      .listen('9000');
+
+      console.log('Server listening on http://localhost:9000');
+    });
+
+// Собираем Stylus
+gulp.task('stylus', function() {
+  return gulp.src(paths.stylus)
+  .pipe(plumber())
+  .pipe(stylus({
+    use: ['nib']
+    })) // собираем stylus
+    .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+    .pipe(concat("style_stylus.css"))
+    .pipe(gulp.dest('./public/css/')) // записываем css
+    .pipe(livereload(server)); // даем команду на перезагрузку css
+  });
+
+// Собираем css
+gulp.task('css', function() {
+  return gulp.src(paths.css)
+  .pipe(plumber())
+    .on('error', console.log) // Если есть ошибки, выводим и продолжаем
+    .pipe(concat("style_css.css"))
+    .pipe(gulp.dest('./public/css/')) // записываем css
+    .pipe(livereload(server)); // даем команду на перезагрузку css
+  });
+
+gulp.task('js', function() {
+  // Minify and copy all JavaScript
+  return gulp.src(paths.js)
+  .pipe(plumber())
+  .on('error', console.log)
+  .pipe(uglify())
+  .pipe(concat('scripts_js.min.js'))
+  .pipe(gulp.dest('public/js'))
+  .pipe(livereload(server));
+});
+
+gulp.task('coffee', function() {
+  // Minify and copy all CoffeeScript
+  return gulp.src(paths.coffee)
+  .pipe(plumber())
+  .pipe(coffee())
+  .on('error', console.log)
+  .pipe(uglify())
+  .pipe(concat('scripts_coffee.min.js'))
+  .pipe(gulp.dest('public/js'))
+  .pipe(livereload(server));
+});
+
+gulp.task('pages', function() {
+  return gulp.src(paths.pages)
+  .pipe(plumber())
+  .on('error', console.log)
+  .pipe(gulp.dest('public'))
+  .pipe(livereload(server));
+});
+
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+  .pipe(plumber())
+  .on('error', console.log)
+  .pipe(gulp.dest('public/img'))
+  .pipe(livereload(server));
+});
+
+gulp.task('watch', function() {
+  //предварительная сборка
+  gulp.run('css');
+  gulp.run('stylus');
+  gulp.run('coffee');
+  gulp.run('js');
+  gulp.run('pages');
+  gulp.run('images');
+
+  // Подключаем Livereload
+  server.listen(35729, function(err) {
+    if (err) return console.log(err);
+
+    gulp.watch(paths.css, function() {
+      gulp.run('css');
+    });
+
+    gulp.watch(paths.stylus, function() {
+      gulp.run('stylus');
+    });
+
+    gulp.watch(paths.coffee, function() {
+      gulp.run('coffee');
+    });
+
+    gulp.watch(paths.js, function() {
+      gulp.run('js');
+    });
+
+    gulp.watch(paths.pages, function() {
+      gulp.run('pages');
+    });
+
+    gulp.watch(paths.images, function() {
+      gulp.run('iamges');
+    });
+  });
+
+  gulp.run('http-server');
+});
+
+gulp.task('default', ['watch']);
